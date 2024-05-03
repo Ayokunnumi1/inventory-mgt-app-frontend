76cb31fc085cf158338fa7aa8515706b1e8af390
Ayokunnumi1 <omololuayk@gmail.com>
2024-05-03 18:17:02 +0100

    Import tailwind entry points

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import store from './store';
 import { Provider } from 'react-redux';
 import App from './App';
+import '../src/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
